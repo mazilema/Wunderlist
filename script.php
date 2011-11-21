26e69ca0d67f8d23bbb546b0a9ab1eee786f80c2
David Ferguson <jdfwarrior@gmail.com>
2011-11-21 09:12:06 -0600

    Initial push of Wunderlist extension

---
+++ script.php
@@ -0,0 +1,91 @@
+<?php
+
+
+	/**
+	* Create new database object, then grab the
+	* user input and save it for parsing
+	*/
+	$db = new WunderDB();
+
+
+	/**
+	* If no task is specified, default to "all tasks" list
+	* If a task is specified, split up the input and continue
+	*/
+	if ($argv[1] == "") { $task = "all tasks"; }
+	else { $task = $argv[1]; }
+	$all = explode(" ",$task);
+
+
+	/**
+	* List Tasks
+	* List all tasks that have not been completed in the Inbox list
+	*/
+	if ($task == "tasks") {
+		$results = $db->query('SELECT * from `tasks` WHERE `list_id`="1"');
+		$inc=1;
+		while ($row = $results->fetchArray()) {
+			echo "¬ ".$row['name']."\r";
+			$inc++;
+		}
+	}
+
+
+	/**
+	* List ALL Tasks
+	* List all tasks that have not been completed
+	*/
+	else if ($task == "all tasks") {
+		$lists = $db->query('SELECT id, name FROM `lists`');
+
+		while ($list = $lists->fetchArray()) {
+			$tasks = $db->query('SELECT name from `tasks` where `list_id`="'.$list['id'].'"');
+			$inc=1;
+			echo $list['name']."\r";
+			while ($task = $tasks->fetchArray()) {
+				echo "¬ ".$task['name']."\r";
+				$inc++;
+			}
+			if ($inc == 1) { echo "¬ No tasks in this list\r"; }
+				
+		}
+		
+	}
+
+
+	/**
+	* Add New Task
+	* Add the input text to Wunderlist as a new task.
+	*/
+	else {
+		$result = $db->query('INSERT into `tasks` (name, list_id, note, done, position ,deleted) VALUES("'.$task.'","1","",0,9,0)');
+		echo "Task added: $task";
+	}
+
+	$db->close();
+
+
+
+
+
+	/**
+	* WunderDB Class
+	* Custom class extending the SQLite class for opening and working with an SQLite database
+	*/
+	class WunderDB extends SQLite3
+	{
+
+	    function __construct()
+	    {
+	    	$user = exec("whoami");
+	    	if (file_exists("/Users/$user/Library/Wunderlist/wunderlist.db")) {
+	    		$this->open("/Users/$user/Library/Wunderlist/wunderlist.db");	
+	    	}
+			else {
+				exit("Unable to find local Wunderlist task database");
+			}
+	    }
+
+	}
+
+?>
